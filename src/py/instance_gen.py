d1521ccd734dc99cd5fc72a42e77c5f91501973e
piotrekpuszcz <piotrekpuszcz@icloud.com>
2022-03-15 17:34:22 +0100

    gen random graphs fix

--- src/py/instance_gen.py
+++ src/py/instance_gen.py
@@ -7,25 +7,25 @@ class InstanceType(Enum):
     EUC2D = 3
 
 def header(filename, filetype, description, dimension, edgeWeightType):
-    return "NAME: " + filename + "\n" +\
-            "TYPE: " + filetype + "\n" +\
-            "COMMENT: " + description + "\n" +\
-            "DIMENSION: " + dimension + "\n" +\
-            "EDGE_WEIGHT_TYPE: " + edgeWeightType + "\n"
+    return f"NAME: {filename}\n" +\
+            f"TYPE: {filetype}\n" +\
+            f"COMMENT: {description}\n" +\
+            f"DIMENSION: {dimension}\n" +\
+            f"EDGE_WEIGHT_TYPE: {edgeWeightType}\n"
 
 def to_atsp(filename, mat, dimension, description, maxVal):
-    f = open("../data/" + filename + ".atsp", "w")
+    f = open(f"../data/{filename}.atsp", "w")
     f.write(header(filename, "ATSP", description, dimension, "EXPLICIT"))
     f.write("EDGE_WEIGHT_FORMAT: FULL_MATRIX\n")
     f.write("EDGE_WEIGHT_SECTION\n")
 
-    maxLen = len(str(maxVal)) + 1
+    maxLen = len(str(maxVal)) + 3
 
     for y in mat:
         for x in y:
             curLen = len(str(x))
             f.write(" " * (maxLen - curLen))
-            f.write(x)
+            f.write(str(x))
         f.write("\n")
 
     f.write("EOF\n")
@@ -33,12 +33,12 @@ def to_atsp(filename, mat, dimension, description, maxVal):
     return None
 
 def to_tsp(filename, coords, dimension, description, maxVal):
-    f = open("../data/" + filename + ".tsp", "w")
+    f = open(f"../data/{filename}.tsp", "w")
     f.write(header(filename, "TSP", description, dimension, "EUC_2D"))
     f.write("NODE_COORD_SECTION\n")
 
     for i in range(dimension):
-        f.write(i + " " + coords[i][0] + " " + coords[i][1] + "\n")
+        f.write(f"{i} {coords[i][0]}.0 {coords[i][1]}.0\n")
 
     f.write("EOF\n")
     f.close()
@@ -65,3 +65,9 @@ def gen_random_instance(filename, dimension, instanceType=InstanceType.SYMETRIC,
         to_atsp(filename, mat, dimension, description, maxVal)
         return mat
         
+
+if __name__ == '__main__':
+    gen_random_instance("test_symetric", 10, InstanceType.SYMETRIC)
+    gen_random_instance("test_euc2d", 10, InstanceType.EUC2D)
+    gen_random_instance("test_asymetric", 10, InstanceType.ASYMETRIC)
+    
